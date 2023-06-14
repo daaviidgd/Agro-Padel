1117b3c1dae6ce25aa33f2bf9e74e0c392c038f2
daaviidgd <DAAVIIDGD@GMAIL.COM>
2023-06-14 14:44:47 +0200

    Final alpha

--- reservar.php
+++ reservar.php
@@ -29,22 +29,28 @@ if (isset($_GET['dia']) && isset($_GET['hora']) && isset($_GET['pista']) && isse
     if ($stmt->execute()) {
         // Mostrar un mensaje emergente en la ventana anterior
         // Mostrar la información de la reserva
-        echo "<h1>Reserva realizada</h1>";
-        echo "<p>Día: $fecha</p>";
-        echo "<p>Hora: $hora:00</p>";
-        echo "<p>Pista: $pista</p>";
+         // Mostrar la información de la reserva con estilos
+        echo '<div style="text-align: center; margin-top: 50px;">';
+        echo '<h1 style="color: blue;">Reserva realizada</h1>';
+        echo '<p style="font-size: 18px;">Día: ' . $fecha . '</p>';
+        echo '<p style="font-size: 18px;">Hora: ' . $hora . ':00</p>';
+        echo '<p style="font-size: 18px;">Pista: ' . $pista . '</p>';
+        echo '</div>';
+        echo '<p style="text-align: center; margin-top: 20px;"><a style="color: blue; font-size: 16px;" href="javascript:history.back()">Volver</a></p>';
 
-        // Enlace para regresar a la página anterior
-        echo '<p><a href="javascript:history.back()">Volver</a></p>';
     } else {
         // Manejar el caso en que ocurra un error al ejecutar la consulta
         echo "Error al realizar la reserva: " . $stmt->error;
     }  
+    // cerrar la conexión a la base de datos
+    $stmt->close();
+    $conn->close();
 }else{
-    echo 'Debes estar loggeado';
-    echo '<p><a href="javascript:history.back()">Volver</a></p>';
+    echo '<div style="text-align: center; margin-top: 20px;">';
+    echo '<p style="color: red; font-size: 16px;">Debes estar loggeado</p>';
+    echo '</div>';
+    echo '<p style="text-align: center; margin-top: 20px;"><a style="color: blue; font-size: 16px;" href="javascript:history.back()">Volver</a></p>';
 }
-// cerrar la conexión a la base de datos
-$stmt->close();
-$conn->close();
+
+
 ?>
